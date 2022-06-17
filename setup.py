678adc29cfa3881e2cb61b81902db813f2ffa051
Conor Bergman <conorbergman@gmail.com>
2022-06-17 11:07:57 -0400

    V 0.1.1

--- setup.py
+++ setup.py
@@ -1,14 +1,32 @@
+"""dj-ratelimit
+
+dj-ratelimit is a redis backed ratelimit for django requests. To use set the following
+environment variables in your django settings:
+
+    - ENVIRONMENT: default 'local'
+    - DJ_RATELIMIT_REDIS_ADDRESS: default 'localhost'
+    - DJ_RATELIMIT_REDIS_PORT: default '6379'
+
+For more information see project github page 'py-ratelimit'
+"""
+
 from setuptools import setup
+
+DOCLINES = (__doc__ or "").split("\n")
 
 setup(
     name='dj_ratelimit',
-    version='0.1.0',
-    description='',
+    version='0.1.1',
+    description='Redis backed library implementing a django ratelimit',
+    long_description="\n".join(DOCLINES[2:]),
     url='https://github.com/conorbergman/py-ratelimit',
     author='Conor Bergman',
     author_email='conorbergman@gmail.com',
     license='',
-    packages=['dj_ratelimit'],
+    packages=[
+        'dj_ratelimit',
+        'dj_ratelimit.tst',
+    ],
     install_requires=[
         'Django>=4.0.5',
         'djangorestframework>=3.12.4',
